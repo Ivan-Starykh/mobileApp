e8bd370f98ec0dfa4e69b78d6410f55e25599684
Ivan-Starykh <izobilny2016@yandex.ru>
2024-06-14 08:36:45 +0300

    customised tabNavigator

--- Navigator/TabNavigator.js
+++ Navigator/TabNavigator.js
@@ -3,19 +3,48 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import CoursesScreen from "../screens/CoursesScreen";
 import ProjectsScreen from "../screens/ProjectsScreen";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const activeColor = "#4775f2";
+const inactiveColor = "#b8bece";
 
 const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "home";
+          } else if (route.name === "Courses") {
+            iconName = "book";
+          } else if (route.name === "Projects") {
+            iconName = "briefcase";
+          }
+
+          return (
+            <Ionicons
+              name={iconName}
+              size={24}
+              color={focused ? activeColor : inactiveColor}
+            />
+          );
+        },
+        tabBarLabel: route.name === "Home" ? "Home" : route.name,
+        tabBarActiveTintColor: activeColor,
+        tabBarInactiveTintColor: inactiveColor,
+      })}
+    >
       <Tab.Screen
-        name="HomeTab"
+        name="Home"
         component={HomeScreen}
         options={{ headerShown: false }}
       />
-      <Tab.Screen name="CoursesTab" component={CoursesScreen} />
-      <Tab.Screen name="ProjectsTab" component={ProjectsScreen} />
+      <Tab.Screen name="Courses" component={CoursesScreen} />
+      <Tab.Screen name="Projects" component={ProjectsScreen} />
     </Tab.Navigator>
   );
 }
